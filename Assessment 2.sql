473b3ed9cd63750abcdd06710f5fbf7932b4984b
edlina99 <74677753+edlina99@users.noreply.github.com>
2023-09-02 15:11:09 +0800

    Add files via upload

---
+++ Assessment 2.sql
@@ -0,0 +1,7 @@
+SELECT PlatformName, Id, PlatformId, UniqueName, Latitude, Longitude, CreatedAt, UpdatedAt
+FROM (
+  SELECT P.UniqeuName AS PlatformName, w.Id, w.PlatformId, w.UniqueName, w.Latitude, w.Longitude, w.CreatedAt, w.UpdatedAt, ROW_NUMBER() OVER (PARTITION BY w.PlatformId ORDER BY w.UpdatedAt DESC) AS rn
+  FROM [dbo].[Wells] w
+  INNER JOIN [dbo].[Platforms] P ON P.Id = w.PlatformId
+) AS subquery
+WHERE rn = 1;
